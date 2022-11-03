8c8bddeb20526b24438e22d7f98d9e8c2dec7986
Thomas <56136360+thom-bahm@users.noreply.github.com>
2022-11-03 12:50:01 -0600

    init
    Initial commit

---
+++ Converter/Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Thomas Bahmandeji on 11/3/22.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
